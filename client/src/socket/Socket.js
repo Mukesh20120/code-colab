1eb1702de778ea798443a742c3070096e49ee544
Mukesh <mukeshsharma20120@gmail.com>
2025-04-10 18:04:06 +0530

    change the url of the server

--- client/src/socket/Socket.js
+++ client/src/socket/Socket.js
@@ -1,6 +1,7 @@
 import {io} from 'socket.io-client'
 
 export default function Socket() {
-    const socket = io('http://localhost:5000',{withCredentials: true});
+    const socket = io('https://code-colab-7l1m.onrender.com:5000',{withCredentials: true});
+    // const socket = io('http://localhost:5000',{withCredentials: true});
     return socket;
 }
